a9fa4dcfe43fba69f5597160975fa58f15bd81bc
Anuj Kumar Sharma <anuj55149@gmail.com>
2025-01-16 21:26:13 +0530

    Part - 3, Error Handling

---
+++ 3_Error_Handling/script.js
@@ -0,0 +1,14 @@
+// const a = 4 + 5;
+
+// throw new Error("pass anything, msg");
+
+// // console.log(b);
+
+try {
+    const a = 4 + 3;
+    console.log(b);
+} catch (ex) {
+    console.log("Anuj is logging", ex);
+} finally {
+    console.log("always be executed");
+}
